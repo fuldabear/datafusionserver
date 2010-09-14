c03cb2ee0b48c70e2c517919dcec4e0ad150c100
David Fullmer <fulda@mech.uta.edu>
2010-09-13 23:11:49 -0400

    more house cleaning, and some new placeholders for new features

---
+++ index.php
@@ -0,0 +1,8 @@
+﻿<?php
+	/*
+		For now this file defaults to the data functions as defined in ./bin/data.php
+
+		In the future this file will determine whether a web-site, wiki-site, or functions within ./bin are presented.
+	*/
+	require_once('./bin/data.php');
+?>
