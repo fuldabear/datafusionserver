2472cbbdb7e5565bc5cb01311f8fb04296b152b5
David Fullmer <fulda@mech.uta.edu>
2010-09-15 14:13:31 -0400

    complete all of the index.php's in each directory

--- database/index.php
+++ database/index.php
@@ -1,1 +1,8 @@
-Access Denied
+﻿<?php
+	/*
+		This file is a security precaution to prevent the potential to bypass security measures by redirecting the 
+		browser back to the main program
+	*/
+	header('Location:http://'.$_SERVER["HTTP_HOST"].'?'.$_SERVER["QUERY_STRING"]);
+	echo 'Access Denied';
+?>
--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 ﻿<?php
 	/*
-		For now this file defaults to the data functions as defined in ./bin/data.php
-
-		In the future this file will determine whether a web-site, wiki-site, or functions within ./bin are presented.
+		For now this file defaults to the data functions as defined in ./bin/data.php if _GET or _POST exist
+		This file determines whether a web-site, wiki-site, or functions within ./bin are presented.
 	*/
 
-
-	if()
+	if(isset($_GET) || isset($_POST))
 	{
 		require_once('./bin/data.php');
 	}
