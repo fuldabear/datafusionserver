d6068be915b8cb00126c8755eb70b914809f841a
David Fullmer <david.fullmer@asdl.gatech.edu>
2010-10-15 01:05:09 -0400

    added json2obj.php

---
+++ lib/json2obj.php
@@ -0,0 +1,47 @@
+<?php
+	class json2obj
+	{
+		var $json = null;
+		var $length = null;
+		var $result = null;
+		
+		function json2obj($j=null){
+			if($j != null){
+				$this->json = $j;
+				$this->decode();
+			}
+		}
+		
+		public function decode($in=null){
+			if($in == null) $in = $this->json;
+			if(!json_decode(str_replace("'",'"',$in)) || is_numeric(json_decode(str_replace("'",'"',$in))) || $in == null) return false;
+			$this->result = json_decode(str_replace("'",'"',$in));
+			$this->length = count($this->result);
+			return $this->result;
+		}
+		
+		public function andString($in=null){
+			if($in == null) return false;
+			if($this->result == null) $this->decode();
+			if($this->result == false) return false;
+			$r = $this->result;
+			$s = "$in='$r[0]' ";
+			for($i = 1; $i < $this->length; $i++){
+				$s .= "AND $in='$r[$i]' ";
+			}
+			return $s;
+		}
+		
+		public function orString($in=null){
+			if($in == null) return false;
+			if($this->result == null) $this->decode();
+			if($this->result == false) return false;
+			$r = $this->result;
+			$s = "$in='$r[0]' ";
+			for($i = 1; $i < $this->length; $i++){
+				$s .= "OR $in='$r[$i]' ";
+			}
+			return $s;
+		}
+	}	
+?>
